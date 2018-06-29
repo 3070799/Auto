d7b96fa07baf643495b771e6e986f856abf7ff1e
Andrei Ponomarenko <3355844@gmail.com>
2018-06-29 18:59:37 +0300

    Change_path_variables

--- src/MainRunner.java
+++ src/MainRunner.java
@@ -3,7 +3,27 @@ import models.Auto;
 public class MainRunner {
 //    psvm Скорочене створення методу Main
     public static void main(String[] args) {
-        Auto autoMercedess = new Auto();
-        autoMercedess.driveForward(5);
+
+        //    Змінні
+        String str = "Рядок";
+        //    Символ
+        char ch = 'c';
+        //     Числа
+        byte by = 22;
+        short sh = 22222;
+        int i = 222222222;
+        long l = 2222222222222222222l;
+        //    Числа зі плаваючою крапкою
+        float f1 = -1.1f;
+        double d1 = -1.1;
+        //    Логічна змінна
+        boolean check = true;
+//
+
+
+        Auto autoMercedes = new Auto();
+        autoMercedes.driveForward(5);
+
+
     }
 }
--- src/models/Auto.java
+++ src/models/Auto.java
@@ -1,26 +1,9 @@
 package models;
 
 public class Auto extends AutoAbs {
-    //    Змінні
-    String str = "Строка";
-    //    Символ
-    char ch = 'c';
-    //     Числа
-    byte by = 22;
-    short sh = 22222;
-    int i = 222222222;
-    long l = 2222222222222222222l;
-    //    Числа зі плаваючою крапкою
-    float f1 = -1.1f;
-    double d1 = -1.1;
-    //    Логічна змінна
-    boolean check = true;
-//
-
 
     @Override
     public void driveForward(int transmissionNumber) {
-
         System.out.print("Number of transmission is:");
         System.out.println(transmissionNumber);
     }
