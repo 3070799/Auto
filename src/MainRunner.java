4a86dfdb18b964ea05ba5d5de045d45a25fca881
Andrei Ponomarenko <3355844@gmail.com>
2018-06-29 20:58:41 +0300

    Methods_covert_numbers

--- src/MainRunner.java
+++ src/MainRunner.java
@@ -3,25 +3,27 @@ import models.Auto;
 public class MainRunner {
     //    psvm Скорочене створення методу Main
     public static void main(String[] args) {
-
-
         //    Числа зі плаваючою крапкою
-        float f1 = -1.1f;
-        double d1 = -1.1;
+        float f1 = 2222222222.922222f;
+        double d1 = 2222222222222222.2;
         //    Логічна змінна
         boolean check = true;
         Integer i1 = 154;
         //    Символ
         char ch = 'c';
         //     Числа
-        byte by = 22;
+        byte by = 127;
         short sh = 22222;
         int i = 222222222;
         long l = 2222222222222222222l;
 
+//        Перетворення змінних
         int temp = (int) l;
+        int convertToIntFromFloat = (int) f1;
+        int convertToIntFromDouble = (int) d1;
+        long convertToLongFromFloat = (long) f1;
+        long convertToLongFromDouble = (long) d1;
         long tempLong = (long) temp;
-        //    Змінні
         String s = String.valueOf(l);
         long convertFromString = Long.parseLong(s);
 
@@ -30,5 +32,9 @@ public class MainRunner {
         autoMercedes.driveForward(l);
         autoMercedes.driveForward(s);
         autoMercedes.driveForward(convertFromString);
+        autoMercedes.driveForward(convertToIntFromFloat);
+        autoMercedes.driveForward(convertToIntFromDouble);
+        autoMercedes.driveForward(convertToLongFromFloat);
+        autoMercedes.driveForward(convertToLongFromDouble);
     }
 }
